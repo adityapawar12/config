8ff23de7aaf773940354d06f6a68dfea7a57cd5f
Aditya Pawar <aditya@mx>
2022-02-08 15:01:34 +0530

    Added the auto file system script.

---
+++ auto-my-fs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+mkdir ~/vicky/
+
+mkdir ~/vicky/pics/
+mkdir ~/vicky/pics/me/
+mkdir ~/vicky/pics/friends/
+mkdir ~/vicky/pics/family/
+mkdir ~/vicky/pics/wallpapers/
+
+mkdir ~/vicky/videos/
+mkdir ~/vicky/videos/anime/
+mkdir ~/vicky/videos/anime/fav/
+mkdir ~/vicky/videos/series/
+mkdir ~/vicky/videos/movies/
+mkdir ~/vicky/videos/tutorials/
+mkdir ~/vicky/videos/tutorials/coding/
+mkdir ~/vicky/videos/tutorials/music-production/
+mkdir ~/vicky/videos/my-videos/
+
+mkdir ~/vicky/other/
+
+mkdir ~/vicky/music/
+mkdir ~/vicky/music/hindi/
+mkdir ~/vicky/music/english/
+mkdir ~/vicky/music/marathi/
+mkdir ~/vicky/music/other/
+
+mkdir ~/vicky/softwares/
+mkdir ~/vicky/softwares/linux/
+mkdir ~/vicky/softwares/linux/appimages/
+mkdir ~/vicky/softwares/linux/debian/
+mkdir ~/vicky/softwares/linux/other/
+mkdir ~/vicky/softwares/windows/
+mkdir ~/vicky/softwares/windows/music-production/
+mkdir ~/vicky/softwares/windows/games/
+mkdir ~/vicky/softwares/windows/coding/
+mkdir ~/vicky/softwares/windows/other/
+
+mkdir ~/vicky/music-production/
+mkdir ~/vicky/music-production/daw/
+mkdir ~/vicky/music-production/daw/caustic/
+mkdir ~/vicky/music-production/daw/fl-studio/
+mkdir ~/vicky/music-production/daw/tracktion/
+mkdir ~/vicky/music-production/daw/ableton/
+mkdir ~/vicky/music-production/synth/
+mkdir ~/vicky/music-production/synth/vital/
+mkdir ~/vicky/music-production/synth/surge/
+mkdir ~/vicky/music-production/synth/helm/
+
+mkdir ~/vicky/coding/
+mkdir ~/vicky/coding/c/
+mkdir ~/vicky/coding/cpp/
+mkdir ~/vicky/coding/python/
+mkdir ~/vicky/coding/web-devlopement/
+mkdir ~/vicky/coding/web-devlopement/angular-proj/
+mkdir ~/vicky/coding/web-devlopement/react-proj/
+mkdir ~/vicky/coding/web-devlopement/node-proj/
+mkdir ~/vicky/coding/web-devlopement/php-proj/
+mkdir ~/vicky/coding/web-devlopement/python-proj/
+mkdir ~/vicky/coding/web-devlopement/vanilla-proj/
+mkdir ~/vicky/coding/c-sharp/
+mkdir ~/vicky/coding/bash/
