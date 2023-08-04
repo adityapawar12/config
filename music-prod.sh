8b66216ae9555eb6e48f0837328327b0e22f987f
aditya pawar <44188899+adityapawar12@users.noreply.github.com>
2023-08-05 03:36:00 +0530

    Create music-prod.sh
    added music prod bash script

---
+++ music-prod.sh
@@ -0,0 +1,20 @@
+sudo pacman -Syu
+sudo pacman -S timeshift
+sudo pacman -S octopi
+sudo pacman -Syu
+which jackd
+which jackdsudo pacman -S jack2
+sudo pacman -S cadence
+groups
+whoami
+sudo gpasswd -a user-name audio
+sudo groupadd audio
+sudo usermod -a -G audio $USER
+
+cd /etc/security
+cd limits.d
+ls
+cd ..
+sudo vi limits.conf
+
+
