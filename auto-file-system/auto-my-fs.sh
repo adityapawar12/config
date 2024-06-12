a201265f5c0bfdc1a5ebc271fd1dd0ffdaef982d
aditya pawar <44188899+adityapawar12@users.noreply.github.com>
2024-06-12 18:40:14 +0530

    Updated auto-my-fs.sh

--- auto-file-system/auto-my-fs.sh
+++ auto-file-system/auto-my-fs.sh
@@ -1,75 +1,9 @@
 #!/bin/bash
 
-mkdir ~/vicky/
+# Create directories
+mkdir -p ~/vicky/{pics,videos,music,softwares,streaming,coding,music-production,other}
 
-mkdir ~/vicky/pics/
-mkdir ~/vicky/pics/me/
-mkdir ~/vicky/pics/friends/
-mkdir ~/vicky/pics/family/
-mkdir ~/vicky/pics/wallpapers/
-
-mkdir ~/vicky/videos/
-mkdir ~/vicky/videos/anime/
-mkdir ~/vicky/videos/anime/fav/
-mkdir ~/vicky/videos/series/
-mkdir ~/vicky/videos/movies/
-mkdir ~/vicky/videos/tutorials/
-mkdir ~/vicky/videos/tutorials/coding/
-mkdir ~/vicky/videos/tutorials/coding/udemy/
-mkdir ~/vicky/videos/tutorials/coding/youtube/
-mkdir ~/vicky/videos/tutorials/music-production/
-mkdir ~/vicky/videos/tutorials/music-production/youtube/
-mkdir ~/vicky/videos/my-videos/
-
-mkdir ~/vicky/other/
-
-mkdir ~/vicky/music/
-mkdir ~/vicky/music/hindi/
-mkdir ~/vicky/music/english/
-mkdir ~/vicky/music/marathi/
-mkdir ~/vicky/music/japanese/
-mkdir ~/vicky/music/other/
-
-mkdir ~/vicky/softwares/
-mkdir ~/vicky/softwares/linux/
-mkdir ~/vicky/softwares/windows/
-mkdir ~/vicky/softwares/windows/games/
-
-mkdir ~/vicky/music-production/
-mkdir ~/vicky/music-production/daw/
-mkdir ~/vicky/music-production/daw/caustic/
-mkdir ~/vicky/music-production/daw/fl-studio/
-mkdir ~/vicky/music-production/daw/lmms/
-mkdir ~/vicky/music-production/daw/ardour/
-mkdir ~/vicky/music-production/daw/waveform/
-mkdir ~/vicky/music-production/daw/ableton/
-mkdir ~/vicky/music-production/synth/
-mkdir ~/vicky/music-production/synth/vital/
-mkdir ~/vicky/music-production/synth/surge/
-mkdir ~/vicky/music-production/synth/helm/
-
-mkdir ~/vicky/coding/
-mkdir ~/vicky/coding/c/
-mkdir ~/vicky/coding/config/
-mkdir ~/vicky/coding/cpp/
-mkdir ~/vicky/coding/java/
-mkdir ~/vicky/coding/kotlin/
-mkdir ~/vicky/coding/android/
-mkdir ~/vicky/coding/flutter/
-mkdir ~/vicky/coding/react-native/
-mkdir ~/vicky/coding/python/
-mkdir ~/vicky/coding/web-devlopement/
-mkdir ~/vicky/coding/web-devlopement/angular-proj/
-mkdir ~/vicky/coding/web-devlopement/reactjs-proj/
-mkdir ~/vicky/coding/web-devlopement/veujs-proj/
-mkdir ~/vicky/coding/web-devlopement/node-proj/
-mkdir ~/vicky/coding/web-devlopement/php-proj/
-mkdir ~/vicky/coding/web-devlopement/python-proj/
-mkdir ~/vicky/coding/web-devlopement/vanilla-proj/
-mkdir ~/vicky/coding/c-sharp/
-mkdir ~/vicky/coding/bash/
-mkdir ~/vicky/coding/zsh/
-mkdir ~/vicky/coding/rust/
-
-mkdir ~/vicky/office
-
+# Create symbolic links
+ln -s ~/vicky/pics ~/Pictures
+ln -s ~/vicky/videos ~/Videos
+ln -s ~/vicky/music ~/Music
